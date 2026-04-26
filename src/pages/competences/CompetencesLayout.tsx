1db9535525d2078aef8f5f69128a270edceced9e
Nathan jouannon <nathan.jouannon@gmail.com>
2026-04-26 22:26:47 +0200

    update compétence style

--- src/pages/competences/CompetencesLayout.tsx
+++ src/pages/competences/CompetencesLayout.tsx
@@ -129,34 +129,36 @@ const competenceGroups: CompetenceGroup[] = [
 export default function CompetencesLayout() {
   return (
     <section className="competences-page" aria-labelledby="competences-title">
-      {competenceGroups.map((group) => (
-        <div key={group.id} className="competences-shell" style={{ marginBottom: 30 }}>
-          <h1 id="competences-title" className="competences-title">
-            {group.title}
-          </h1>
+      <div className="competences-shell">
+        <h1 id="competences-title" className="competences-title">
+          Compétence
+        </h1>
 
-          <ul
-            className={`competence-list ${
-              group.id === 'human-skills-title'
-                ? 'competence-list-human'
-                : 'competence-list-technical'
-            }`}
-          >
-            {group.items.map((item) => (
-              <li key={item.to} className="competence-item">
-                <Link className="competence-link" to={item.to}>
-                  <span className="competence-logo" aria-hidden="true">
-                    <svg className="competence-icon" viewBox="0 0 24 24" fill="none">
-                      {item.icon}
-                    </svg>
-                  </span>
-                  <span className="competence-label">{item.label}</span>
-                </Link>
-              </li>
-            ))}
-          </ul>
+        <div className="competences-grid">
+          {competenceGroups.map((group) => (
+            <div key={group.id} className="competence-block">
+              <h2 id={group.id} className="competence-block-title">
+                {group.title}
+              </h2>
+
+              <ul aria-labelledby={group.id} className="competence-list">
+                {group.items.map((item) => (
+                  <li key={item.to} className="competence-item">
+                    <Link className="competence-link" to={item.to}>
+                      <span className="competence-logo" aria-hidden="true">
+                        <svg className="competence-icon" viewBox="0 0 24 24" fill="none">
+                          {item.icon}
+                        </svg>
+                      </span>
+                      <span className="competence-label">{item.label}</span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </section>
   )
 }
