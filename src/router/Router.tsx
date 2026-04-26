240281359eee1368ed1b62c1cb6f1574c36b73c9
Nathan jouannon <nathan.jouannon@gmail.com>
2026-04-26 23:33:34 +0200

    update router to have autoScroll to top

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from '../pages/Home/Home'
 import Contact from '../pages/Contact/Contact'
 import Career from '../pages/Career/Career'
@@ -7,16 +8,36 @@ import Competence from '../pages/competences/Competence'
 import ProjectLayout from '../pages/projects/ProjectLayout'
 import Projet from '../pages/projects/Projet'
 
+function ScrollToTopOnTargetPages() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    const shouldScrollToTop =
+      pathname.startsWith('/competences') ||
+      pathname === '/projets' ||
+      pathname.startsWith('/projet/')
+
+    if (shouldScrollToTop) {
+      window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
+    }
+  }, [pathname])
+
+  return null
+}
+
 export default function Router() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/career" element={<Career />} />
-      <Route path="/competences" element={<CompetencesLayout />} />
-      <Route path="/competences/:competence" element={<Competence />} />
-      <Route path="/projets" element={<ProjectLayout />} />
-      <Route path="/projet/:slug" element={<Projet />} />
-    </Routes>
+    <>
+      <ScrollToTopOnTargetPages />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/career" element={<Career />} />
+        <Route path="/competences" element={<CompetencesLayout />} />
+        <Route path="/competences/:competence" element={<Competence />} />
+        <Route path="/projets" element={<ProjectLayout />} />
+        <Route path="/projet/:slug" element={<Projet />} />
+      </Routes>
+    </>
   )
 }
